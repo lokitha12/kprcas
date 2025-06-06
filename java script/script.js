86a3f4dc455e7fd97274b1277bae6df7b65cbb3c
lokitha <lokithathangavel1206@gmail.com>
2025-06-06 16:38:11 +0530

    made changes

---
+++ java script/script.js
@@ -0,0 +1,19 @@
+// odd oe even
+var number =57;
+if(number % 2==0){ 
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+console.log("marks")
+var marks=[ 89,89,92,94]
+var[m1,m2,m3,m4]=marks
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+var a=10
+var b=20
+var sum=a+b;
+console.log(a,"+",b,"=",sum)

a44fbc5c5d262f7aa7ef35e89043d6dd2bf2da0f
lokitha <lokithathangavel1206@gmail.com>
2025-06-09 16:29:31 +0530

    made changes

---
+++ kpr/python/condition.py
@@ -0,0 +1,35 @@
+"""a= 20
+b=20
+c=35
+if a == b or b==c:
+    print("a is equal")
+elif b < c or a>b :
+    print("c is greater")
+else:
+    print("b is greater")
+if a == b and b==c:
+    print("a is equal")
+elif b < c and a>b :
+    print("c is greater")
+else:
+    print("b is greater")
+for i in range(5):
+    print(i)
+for i in range(1,6,2):
+    print(i)
+def add():
+    print(145+700)
+add()"""
+list=[27,67,89,90]; 
+for i in list:
+    print(i)
+    print(type(i))
+tuple=(23,45,79,90)
+for i in tuple:
+    print(i)
+    print(type(i))
+dictionary={"name":"loki","password":"shaanvi"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
---
+++ kpr/python/demo.py
@@ -0,0 +1,11 @@
+c="welcome to python"
+a= 20
+b=20
+c=35
+print(a-b)
+print(type(c))
+print(type(a))
+d=True
+print(type(d))
+s=2.14
+print(type(s))
